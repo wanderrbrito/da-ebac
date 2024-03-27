ad564025205574eda2064833e0a1f508ff362834
wanderrbrito <wanderrbrito@gmail.com>
2024-03-27 00:56:17 +0000

    Tarefa do Modulo 19

--- gasolina.py
+++ gasolina.py
@@ -1,12 +1,16 @@
+# código de geração do gráfico 
 # Importando as bibliotecas
 import csv
 import pandas as pd
 import matplotlib.pylab as plt
+import seaborn as sns
 
 # Lendo arquivo CSV e criando Dataframe
 preco_gasolina_df = pd.read_csv('gasolina.csv')
+preco_gasolina_df = preco_gasolina_df.rename(columns={'venda':'preco'})
 preco_gasolina_df
 
 # Gerando grafico de linhas e salvado o grafico como .png
-preco_gasolina_df.plot('dia',"venda")
+sns.lineplot(data=preco_gasolina_df, x='dia', y='preco', color='blue')
+plt.title('Variacao do Preço da Gasolina por Dia')
 plt.savefig('gasolina.png')
